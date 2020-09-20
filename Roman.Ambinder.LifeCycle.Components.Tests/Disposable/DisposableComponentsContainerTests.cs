64a13067224e162ce553e1783bef34624ecba838
Roman Ambinder <roman.ambinder@gmail.com>
2020-09-20 16:25:19 +0300

    Minior project changes

---
+++ Roman.Ambinder.LifeCycle.Components.Tests/Disposable/DisposableComponentsContainerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.TestPlatform.Common.ExtensionFramework.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Roman.Ambinder.LifeCycleComponents.Disposable;
+
+namespace Roman.Ambinder.LifeCycle.Components.Tests.Disposable
+{
+    [TestClass]
+    public class DisposableComponentsContainerTests
+    {
+
+        [TestMethod]
+        public void Test()
+        {
+            //Arrange 
+            using (var container = new DisposableComponentsContainer())
+            {
+
+            }
+
+            //Act 
+
+            //Assert
+        }
+    }
+
+}
